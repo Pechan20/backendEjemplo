79052c0c1a3422ab5878ea54502cc98598bec128
Pechan20 <jr_contreras20@hotmail.com>
2024-08-13 18:24:51 -0600

    se agrega versionamiento de java

---
+++ ejemplo1/src/main/java/com/mycompany/ejemplo1/ejemplo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejemplo1;
+
+/**
+ *
+ * @author jr_co
+ */
+public class ejemplo {
+    //psvm ctrl+space
+    public static void main(String[] args) {
+        //System.out.println(""); sout ctrl+space 
+        System.out.println("hola mundo");
+    }
+}
